a9480cad07a207ca4432a780a9651978ffb36629
Lyq <2058712589@qq.com>
2026-09-10 09:36:11 +0800

    第一个js程序

--- JsStudy/02Js/test.js
+++ JsStudy/02Js/test.js
@@ -1,9 +1,7 @@
 // 定义变量
-let name = "小明";
 const age = 18;
 
 // 输出
-console.log("我叫" + name + "，今年" + age + "岁");
+console.log(`我叫xx，今年${age}岁`);
 
-// 模板字符串（更推荐）
-console.log(`我叫${name}，今年${age}岁`);
+
